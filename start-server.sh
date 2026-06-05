88e16514e1f8bca7dfd9bd8a0f6d36e344ab27f9
Z User <z@container>
2026-06-05 15:18:01 +0000

    1684bffd-8c5b-4805-ae4f-3c81df140fba

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Next.js Dev Server Keeper - auto-restarts when process dies
 cd /home/z/my-project
-LOG=/home/z/my-project/dev.log
-
-while true; do
-  echo "[$(date '+%H:%M:%S')] Starting Next.js dev server..." >> "$LOG"
-  NODE_OPTIONS="--max-old-space-size=512" npx next dev -p 3000 >> "$LOG" 2>&1
-  EXIT_CODE=$?
-  echo "[$(date '+%H:%M:%S')] Server exited (code=$EXIT_CODE). Restarting in 2s..." >> "$LOG"
-  sleep 2
-done
+exec npx next dev -p 3000
